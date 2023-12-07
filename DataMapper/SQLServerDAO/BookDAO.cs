cb55f22c99cfac45233d5b96c4e158dcbf792e47
razz <razzrozo10@gmail.com>
2023-12-07 21:28:40 +0200

    added BookDAO and IBookDAO

---
+++ DataMapper/SQLServerDAO/BookDAO.cs
@@ -0,0 +1,37 @@
+﻿using Library.models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataMapper
+{
+    internal class BookDAO : IBookDataServices
+    {
+        public void AddBook(Book book)
+        {
+            using (var context = new LibraryContext())
+            {
+                context.Books.Add(book);
+                context.SaveChanges();
+            }
+
+        }
+
+        public void DeleteBook(Book book)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Book GetBookById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateBook(Book book)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
