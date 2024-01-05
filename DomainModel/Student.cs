73e34499de41b448ca9d4b6526c48a67efbc63f8
RaZzV2 <razzrozo10@gmail.com>
2024-01-05 19:41:55 +0200

    added unit-tests for student

--- DomainModel/Student.cs
+++ DomainModel/Student.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,7 +9,21 @@ namespace Library.models
 {
     public class Student
     {
-        public int Id { get; set; } 
-        public string Name { get; set; }   
+        public int Id { get; set; }
+
+
+        [Required(ErrorMessage = "First name is required!")]
+        [StringLength(50, MinimumLength = 4, ErrorMessage = "First name must have at least 4 characters!")]
+        [RegularExpression("^[a-zA-Z0-9]+$", ErrorMessage = "First name must not have special characters!")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Last name is required!")]
+        [StringLength(50, MinimumLength = 4, ErrorMessage = "Last name must have at least 4 characters!")]
+        [RegularExpression("^[a-zA-Z0-9]+$", ErrorMessage = "Last name must not have special characters!")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Age is required!")]
+        [Range(7,99, ErrorMessage = "You must have at least 7 and at most 99 years to borrow a book!")]
+        public int Age { get; set; }
     }
 }
--- TestDomainModel/UnitTest1.cs
+++ TestDomainModel/UnitTest1.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Library.models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace TestDomainModel
 {
@@ -7,8 +9,51 @@ namespace TestDomainModel
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void StudentAgeToLow()
         {
+            var user = new Student
+            {
+                FirstName = "Test",
+                LastName = "Testare",
+                Age = 3,
+            };
+            var validationContext = new ValidationContext(user, null, null);
+            var exception = Assert.ThrowsException<ValidationException>(() => Validator.ValidateObject(user, validationContext, true));
+            Assert.AreEqual("You must have at least 7 and at most 99 years to borrow a book!", exception.Message);
+        }
+
+        [TestMethod]
+        public void StudentAgeToHigh()
+        {
+            var user = new Student
+            {
+                FirstName = "Test",
+                LastName = "Testare",
+                Age = 101,
+            };
+            var validationContext = new ValidationContext(user, null, null);
+            var exception = Assert.ThrowsException<ValidationException>(() => Validator.ValidateObject(user, validationContext, true));
+            Assert.AreEqual("You must have at least 7 and at most 99 years to borrow a book!", exception.Message);
+        }
+
+        [TestMethod]
+        public void StudentAgeCorrect()
+        {
+            var user = new Student
+            {
+                FirstName = "Test",
+                LastName = "Testare",
+                Age = 33,
+            };
+            var validationContext = new ValidationContext(user, null, null);
+            try
+            {
+                Validator.ValidateObject(user, validationContext, true);
+            }
+            catch (ValidationException ex)
+            {
+                Assert.Fail($"An exception has been thrown!: {ex.Message}");
+            }
         }
     }
 }
