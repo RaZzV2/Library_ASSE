a5c6dd31abfe93ce62c7d2c03f871b2d40b16027
RaZzV2 <razzrozo10@gmail.com>
2024-01-06 18:42:42 +0200

    added validators and tests for reader class

---
+++ DomainModel/CustomValidators/PhoneOrEmailReq.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainModel
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    public class PhoneOrEmailReq : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var reader = (Reader)validationContext.ObjectInstance;
+
+            if (string.IsNullOrWhiteSpace(reader.PhoneNumber) && string.IsNullOrWhiteSpace(reader.EmailAddress))
+            {
+                return new ValidationResult("Either phone number or email address is required.");
+            }
+            
+            return ValidationResult.Success;
+        }
+    }
+}
--- DomainModel/Reader.cs
+++ DomainModel/Reader.cs
@@ -1,25 +1,44 @@
 ﻿using Library.models;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DomainModel
 {
-    public class Reader
+    [PhoneOrEmailReq(ErrorMessage = "Either phone number or email address is required.")]
+    public class Reader 
     {
         public int ReaderId { get; set; }
 
+
+        [Required(ErrorMessage = "First name is required!")]
+        [StringLength(50, MinimumLength = 4, ErrorMessage = "First name must have at least 4 characters!")]
+        [RegularExpression("^[a-zA-Z0-9]+$", ErrorMessage = "First name must not have special characters!")]
         public string ReaderFirstName { get; set; }
 
-        public string ReaderLastName { get;set; }  
 
-        public string Address { get; set; } 
+        [Required(ErrorMessage = "Last name is required!")]
+        [StringLength(50, MinimumLength = 4, ErrorMessage = "Last name must have at least 4 characters!")]
+        [RegularExpression("^[a-zA-Z0-9]+$", ErrorMessage = "Last name must not have special characters!")]
+        public string ReaderLastName { get;set; }
 
+
+        [Required(ErrorMessage = "Address is required!")]
+        public string Address { get; set; }
+
+
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
         public string EmailAddress { get; set; }
 
+
+        [Required(ErrorMessage = "Role is required!")]
         public bool Role { get; set; }
+
+       
+        [Phone(ErrorMessage = "Invalid Phone Number!")]
         public string PhoneNumber { get; set; }
     }
 }
